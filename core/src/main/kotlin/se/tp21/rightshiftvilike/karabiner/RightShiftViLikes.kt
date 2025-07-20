22656cc9803c02380ae61e87026d53571eec35eb
Tom Power <tp21@tuta.io>
2025-07-20 09:44:39 +0100

    add rightShiftViLikeExtrasEditing.kt

--- core/src/main/kotlin/se/tp21/rightshiftvilike/karabiner/RightShiftViLikes.kt
+++ core/src/main/kotlin/se/tp21/rightshiftvilike/karabiner/RightShiftViLikes.kt
@@ -1,6 +1,7 @@
 package se.tp21.rightshiftvilike.karabiner
 
 import se.tp21.rightshiftvilike.karabiner.modifications.rightShiftViLike
+import se.tp21.rightshiftvilike.karabiner.modifications.rightShiftViLikeExtrasEditing
 import se.tp21.rightshiftvilike.karabiner.modifications.rightShiftViLikeExtrasNavigation
 import se.tp21.rightshiftvilike.karabiner.modifications.rightShiftViLikeExtrasSymbols
 import se.tp21.rightshiftvilike.karabiner.modifications.rightShiftViLikeExtrasTerminal
@@ -9,6 +10,7 @@ import sh.kau.karabiner.ComplexModifications
 fun rightShiftViLikeModificationsMap(): Map<String, ComplexModifications> =
     mapOf(
         "rightShiftViLike" to rightShiftViLike(),
+        "rightShiftViLikeExtrasEditing" to rightShiftViLikeExtrasEditing(),
         "rightShiftViLikeExtrasNavigation" to rightShiftViLikeExtrasNavigation(),
         "rightShiftViLikeExtrasSymbols" to rightShiftViLikeExtrasSymbols(),
         "rightShiftViLikeExtrasTerminal" to rightShiftViLikeExtrasTerminal(),
